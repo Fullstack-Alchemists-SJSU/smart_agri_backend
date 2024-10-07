0836606084aebb96590788e17078452a5dc07913
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2024-10-07 20:44:24 +0100

    Removed NPK reading

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,33 @@
-import getMoisture from "./data-collection/moisture";
-import sendData from "./data-collection/network";
-import getTemperature from "./data-collection/temperature";
-import * as dotenv from "dotenv";
+import getMoisture from "./data-collection/moisture"
+import sendData from "./data-collection/network"
+import getTemperature from "./data-collection/temperature"
+import * as dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
 type ThingSpeakData = {
-  temp: any,
-  moisture: any
-};
+	temp: any
+	moisture: any
+}
 
-const dataQueue: ThingSpeakData[] = [];
-
-setInterval(async () => {
-  try {
-    const temp = await getTemperature();
-    const moisture = await getMoisture();
-
-    console.log(temp, moisture);
-
-    dataQueue.push({ temp, moisture });
-
-    console.log(`Added {${temp}, ${moisture}} to queue`);
-  } catch (error) {
-    console.error("Error reading sensor data:", error);
-  }
-}, 1000);
+const dataQueue: ThingSpeakData[] = []
 
 setInterval(async () => {
-  if (dataQueue.length > 0) {
-    const data = dataQueue.shift();
-    await sendData(data?.temp, data?.moisture);
-  }
-}, 15000);
+	try {
+		const temp = await getTemperature()
+		const moisture = await getMoisture()
+
+		dataQueue.push({temp, moisture})
+
+		console.log(`Added {${temp}, ${moisture}} to queue`)
+	} catch (error) {
+		console.error("Error reading sensor data:", error)
+	}
+}, 1000)
+
+setInterval(async () => {
+	if (dataQueue.length > 0) {
+		const data = dataQueue.shift()
+		await sendData(data?.temp, data?.moisture)
+	}
+}, 15000)
